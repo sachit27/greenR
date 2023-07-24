1b4119134dfaa1c2e6ed924820a0b647bd561db1
Sachit Mahajan <smahajan@gess-rz-dock-1-125.ethz.ch>
2023-07-24 16:59:01 +0200

    leaflet maps

--- R/save_as_leaflet.R
+++ R/save_as_leaflet.R
@@ -1,43 +1,43 @@
 #' Save the green index data as a Leaflet map in an HTML file
 #'
-#' This function saves the green index data for the edges as a Leaflet map in an HTML file.
-#'
-#' @param green_index A data frame containing the calculated green index values for each edge.
+#' @param edges A data frame containing the calculated green index values for each edge.
 #' @param file_path The file path where the HTML file will be saved.
-#' @return NULL
-#' @examples
-#' save_as_leaflet(green_index, "green_index_map.html")
-save_as_leaflet <- function(green_index, file_path) {
-  library(leaflet)
-  library(sf)
-  library(htmlwidgets)
-  library(leaflet.extras)
+#' @importFrom leaflet addProviderTiles addPolylines addLegend leaflet
+#' @importFrom sf st_as_sf st_transform
+#' @importFrom htmlwidgets saveWidget
+#' @importFrom leaflet.extras colorNumeric
+#' @export
+save_as_leaflet <- function(edges, file_path) {
+  # Convert to an sf object and transform the data to the WGS84 CRS
+  edges_sf <- sf::st_as_sf(edges) %>%
+    sf::st_transform(crs = 4326)
 
-  # Convert to an sf object
-  green_index <- st_as_sf(green_index)
+  # Create a continuous color palette
+  color_palette <- leaflet.extras::colorNumeric(palette = c("#F0BB62", "#BFDB38", "#367E18"),
+                                                domain = edges_sf$green_index)
 
-  # Transform the data to the WGS84 CRS
-  edges_sf <- sf::st_transform(green_index, crs = 4326)
+  # Create labels
+  labels <- sprintf(
+    "<strong>Green index:</strong><br/>%.2f",
+    edges_sf$green_index
+  ) %>% lapply(htmltools::HTML)
 
   # Create a Leaflet map
-  map <- leaflet(data = edges_sf) %>%
-    leaflet::addProviderTiles(providers$OpenStreetMap)
-
-  # Add layers of polylines for each level of the green index
-  color_palette <- c("#F0BB62", "#BFDB38", "#367E18")
-  for (i in 1:length(color_palette)) {
-    map <- map %>%
-      leaflet::addPolylines(data = edges_sf[edges_sf$green_index == i, ],
-                            color = color_palette[i],
-                            weight = 1,
-                            opacity = 1,
-                            label = ~ paste("Green Index:", green_index))
-  }
-
-  # Add scale bar
-  map <- map %>% leaflet::addScaleBar(position = "bottomleft")
+  map <- leaflet::leaflet(edges_sf) %>%
+    leaflet::addProviderTiles(providers$OpenStreetMap) %>%
+    leaflet::addPolylines(color = ~color_palette(green_index),
+                          weight = 1,
+                          opacity = 1,
+                          label = ~labels,
+                          highlightOptions = highlightOptions(weight = 5, color = "#666", fillOpacity = 0.7),
+                          group = "edges") %>%
+    leaflet::addLegend("bottomright",
+                       pal = color_palette,
+                       values = ~green_index,
+                       title = "Green Index",
+                       opacity = 1)
 
   # Save the map as an HTML file
-  htmlwidgets::saveWidget(map, file = file_path)   
+  htmlwidgets::saveWidget(map, file = file_path)
   invisible()
 }
