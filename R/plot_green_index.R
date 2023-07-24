1e080a759666a7fb0449bb7a976fd0fa1a510763
Sachit Mahajan <sachitmahajan90@gmail.com>
2023-07-24 12:15:52 +0200

    Update plot_green_index.R

--- R/plot_green_index.R
+++ R/plot_green_index.R
@@ -1,17 +1,18 @@
-#' Plot the green index
-#'
-#' This function plots the green index for the highway network.
-#'
-#' @param green_index A data frame containing the calculated green index values for each edge.
-#' @return NULL
-#' @examples
-#' plot_green_index(green_index)
 plot_green_index <- function(green_index) {
   library(ggplot2)
+  library(sf)  # Add this line
+
+  # Debugging prints
+  print(head(green_index))  # Print the first few rows of green_index
 
   # Define the color range for the gradient
   colors <- c("#F0BB62", "#BFDB38", "#367E18")
   edges_sf <- sf::st_as_sf(green_index)
+
+  # Debugging prints
+  print(class(edges_sf))  # Print the class of edges_sf
+  print(names(edges_sf))  # Print the column names of edges_sf
+
   text_size <- 14
   resolution <- 350
 
