ff93023a277660d531c6176eae58f27566d751d0
Sachit Mahajan <sachitmahajan90@gmail.com>
2023-07-30 13:44:58 +0200

    Update save_as_json.R

--- R/save_as_json.R
+++ R/save_as_json.R
@@ -5,8 +5,17 @@
 #' @param green_index A data frame containing the calculated green index values for each edge.
 #' @param file_path The file path where the JSON file will be saved.
 #' @return NULL
+#' @importFrom jsonlite write_json
 #' @examples
+#' \dontrun{
+#' # Generate a sample green_index data frame
+#' green_index <- data.frame(
+#'   green_index = runif(1000),
+#'   geometry = rep(sf::st_sfc(sf::st_point(c(0, 0))), 1000)
+#' )
 #' save_json(green_index, "green_index_data.json")
+#' }
+#' @export
 save_json <- function(green_index, file_path) {
   jsonlite::write_json(green_index, file_path)
   invisible()
