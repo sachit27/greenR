693d86f9a91e7f97cc271a3d53e3b60746ddc19b
Sachit Mahajan <smahajan@gess-rz-dock-1-125.ethz.ch>
2023-08-05 15:25:48 +0200

    customize_plots

--- R/plot_green_index.R
+++ R/plot_green_index.R
@@ -1,11 +1,28 @@
 #' Plot the green index
 #'
-#' This function plots the green index for the highway network.
+#' This function plots the green index for the highway network with extensive customization options.
+#' Users can set various parameters like text size, color palette, resolution, theme, line width, line type, and more.
 #'
 #' @param green_index A data frame containing the calculated green index values for each edge.
-#' @return NULL
-#' @importFrom ggplot2 ggplot geom_sf scale_color_gradientn theme_minimal theme element_text aes
+#' @param colors Character vector, colors for the gradient. Default is c("#F0BB62", "#BFDB38", "#367E18").
+#' @param text_size Numeric, size of the text in the plot. Default is 14.
+#' @param resolution Numeric, resolution of the plot. Default is 350.
+#' @param title Character, title for the plot. Default is NULL.
+#' @param xlab Character, x-axis label for the plot. Default is NULL.
+#' @param ylab Character, y-axis label for the plot. Default is NULL.
+#' @param legend_title Character, legend title for the plot. Default is NULL.
+#' @param legend_position Character, legend position for the plot. Default is "right".
+#' @param theme ggplot theme object, theme for the plot. Default is ggplot2::theme_minimal().
+#' @param line_width Numeric, width of the line for the edges. Default is 1.
+#' @param line_type Character or numeric, type of the line for the edges. Default is "solid".
+#' @param interactive Logical, whether to return an interactive plot using plotly. Default is FALSE.
+#' @param filename Character, filename to save the plot. Supported formats include png, pdf, jpeg, etc. Default is NULL (no file saved).
+#'
+#' @importFrom ggplot2 ggplot geom_sf scale_color_gradientn theme_minimal theme element_text aes labs
 #' @importFrom sf st_as_sf
+#' @importFrom plotly ggplotly
+#'
+#' @return NULL
 #' @examples
 #' \dontrun{
 #' # Generate a sample green_index data frame
@@ -16,20 +33,43 @@
 #' plot_green_index(green_index)
 #' }
 #' @export
-plot_green_index <- function(green_index) {
+plot_green_index <- function(green_index,
+                             colors = c("#F0BB62", "#BFDB38", "#367E18"),
+                             text_size = 14,
+                             resolution = 350,
+                             title = NULL,
+                             xlab = NULL,
+                             ylab = NULL,
+                             legend_title = NULL,
+                             legend_position = "right",
+                             theme = ggplot2::theme_minimal(),
+                             line_width = 0.5,
+                             line_type = "solid",
+                             interactive = FALSE,
+                             filename = NULL) {
 
-  # Define the color range for the gradient
-  colors <- c("#F0BB62", "#BFDB38", "#367E18")
   edges_sf <- sf::st_as_sf(green_index)
-  text_size <- 14
-  resolution <- 350
 
   plot <- ggplot2::ggplot() +
-    ggplot2::geom_sf(data = edges_sf, ggplot2::aes(color = green_index), size = 0.5) +
+    ggplot2::geom_sf(data = edges_sf, ggplot2::aes(color = green_index), lwd = line_width, linetype = line_type) +
     ggplot2::scale_color_gradientn(colors = colors) +
-    ggplot2::theme_minimal() +
-    ggplot2::theme(text = ggplot2::element_text(size = text_size))
+    theme +
+    ggplot2::theme(text = ggplot2::element_text(size = text_size)) +
+    ggplot2::labs(title = title, x = xlab, y = ylab, color = legend_title) +
+    ggplot2::theme(legend.position = legend_position)
 
+  # Render interactive plot if requested
+  if (interactive) {
+    plot <- plotly::ggplotly(plot)
+  }
+
+  # Print the plot
   print(plot)
+
+  # Save the plot if filename is provided
+  if (!is.null(filename)) {
+    ggplot2::ggsave(filename, plot, dpi = resolution)
+  }
+
   invisible()
 }
