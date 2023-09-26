a5d761ffa5cc4b7b621331ef13a288c5001d5b1b
sachit27 <sachitmahajan90@gmail.com>
2023-09-26 18:55:59 +0200

    Fix bug to save data in geojson format

--- R/save_as_json.R
+++ R/save_as_json.R
@@ -1,11 +1,11 @@
-#' Save the green index data as a JSON file
+#' Save the green index data as a GeoJSON file
 #'
-#' This function saves the green index data for all the edges as a JSON file.
+#' This function saves the green index data for all the edges as a GeoJSON file.
 #'
 #' @param green_index A data frame containing the calculated green index values for each edge.
-#' @param file_path The file path where the JSON file will be saved.
+#' @param file_path The file path where the GeoJSON file will be saved.
 #' @return NULL
-#' @importFrom jsonlite write_json
+#' @importFrom sf st_write
 #' @examples
 #' \dontrun{
 #' # Generate a sample green_index data frame
@@ -13,10 +13,15 @@
 #'   green_index = runif(1000),
 #'   geometry = rep(sf::st_sfc(sf::st_point(c(0, 0))), 1000)
 #' )
-#' save_json(green_index, "green_index_data.json")
+#' save_json(green_index, "green_index_data.geojson")
 #' }
 #' @export
 save_json <- function(green_index, file_path) {
-  jsonlite::write_json(green_index, file_path)
+  # Ensure the data frame is of class 'sf'
+  if(!inherits(green_index, "sf")) {
+    green_index <- sf::st_as_sf(green_index)
+  }
+  # Save as GeoJSON
+  sf::st_write(green_index, file_path, driver = "GeoJSON")
   invisible()
 }
