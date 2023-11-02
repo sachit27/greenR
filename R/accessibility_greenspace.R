813cf720fa82ed74d6948c4b54cdeefa27525102
sachit27 <sachitmahajan90@gmail.com>
2023-11-02 01:13:08 +0100

    Version Update

--- R/accessibility_greenspace.R
+++ R/accessibility_greenspace.R
@@ -1,32 +1,34 @@
 #' Generate Accessibility Map for Green Spaces
 #'
-#' This function generates a map that shows green spaces accessible within a specified
-#' walking time from a given location. The location is specified by its latitude and longitude.
+#' This function generates a leaflet map that shows green spaces accessible within
+#' a specified walking time from a given location. The location is specified by its
+#' latitude and longitude coordinates.
 #'
 #' @param green_area_data A list containing green area data, usually obtained from the \code{get_osm_data} function.
 #' @param location_lat Numeric latitude of the specified location.
 #' @param location_lon Numeric longitude of the specified location.
 #' @param max_walk_time Maximum walking time in minutes. Default is 15.
-#' @param green_color Color for the green areas on the map. Default is "black".
+#' @param green_color Color for the green areas on the map. Default is "green".
 #' @param location_color Color for the specified location on the map. Default is "blue".
 #' @param isochrone_color Color palette for the isochrone lines. Default is "viridis".
 #'
-#' @return A tmap object representing the accessibility map.
+#' @return A leaflet map object.
 #'
-#' @importFrom sf st_as_sf st_transform st_intersection st_make_valid
-#' @importFrom tibble tibble
+#' @importFrom sf st_as_sf st_transform st_make_valid
+#' @importFrom leaflet leaflet addProviderTiles addPolygons addCircles addLegend addLayersControl
 #' @importFrom osrm osrmIsochrone
-#' @importFrom tmap tm_shape tm_polygons tm_dots
-#' @importFrom dplyr rename
-#' @importFrom magrittr %>%
+#' @importFrom tibble tibble
+#'
+#' @examples
+#' \dontrun{
+#'   green_area_data <- data$green_areas
+#'   accessibility_greenspace(data, 47.56, 7.59)
+#' }
+#'
 #' @export
-
 accessibility_greenspace <- function(green_area_data, location_lat, location_lon,
-                                     max_walk_time = 5, green_color = "green",
-                                     location_color = "blue", isochrone_color = "viridis") {
-
-  # Set tmap mode to view for interactive leaflet map
-  tmap::tmap_mode("view")
+                       max_walk_time = 15, green_color = "green",
+                       location_color = "blue", isochrone_color = "viridis") {
 
   # Error Handling: Check if latitude and longitude are numeric and within valid range
   if (!is.numeric(location_lat) || location_lat < -90 || location_lat > 90) {
@@ -42,32 +44,56 @@ accessibility_greenspace <- function(green_area_data, location_lat, location_lon
 
   # Prepare the specified location as an sf object
   specified_location <-
-    tibble::tibble(
+    tibble(
       case_id = c(1),
       lat = c(location_lat),
       lon = c(location_lon)
     ) %>%
-    sf::st_as_sf(., coords = c("lon", "lat"), crs = 4326, agr = "constant") %>%
-    sf::st_transform(., crs = 4326)
+    st_as_sf(., coords = c("lon", "lat"), crs = 4326, agr = "constant") %>%
+    st_transform(., crs = 4326)
 
   # Create the isochrone map using the specified location and walking time
-  iso_map <- osrm::osrmIsochrone(loc = specified_location,
-                                 breaks = seq(from = 0, to = max_walk_time, by = 5),
-                                 osrm.profile = "foot")
+  iso_map <- osrmIsochrone(loc = specified_location,
+                           breaks = seq(from = 0, to = max_walk_time, by = 5),
+                           osrm.profile = "foot")
 
   # Make the isochrone map valid for plotting
-  iso_map_valid <- sf::st_make_valid(iso_map)
+  iso_map_valid <- st_make_valid(iso_map)
 
-  # Find green spaces within the isochrone
-  green_within_isochrone <- sf::st_intersection(osm_sf, iso_map_valid)
+  # Create a label for each isochrone layer
+  iso_map_valid$label <- paste(iso_map_valid$isomax, "minutes")
 
-  # Generate and return the map
-  return(
-    tmap::tm_shape(iso_map_valid %>% dplyr::rename(walk_time = isomax)) +
-      tmap::tm_polygons("walk_time", palette = isochrone_color, alpha = 0.6) +
-      tmap::tm_shape(green_within_isochrone) +
-      tmap::tm_dots(size = 0.1, col = green_color) +
-      tmap::tm_shape(specified_location) +
-      tmap::tm_dots(size = 0.2, col = location_color)
-  )
+  # Generate leaflet map
+  map <- leaflet() %>%
+    addProviderTiles(providers$OpenStreetMap) %>%
+    addPolygons(
+      data = osm_sf,
+      fillColor = green_color,
+      fillOpacity = 1,
+      color = "black",
+      weight = 1,
+      group = "Green Spaces"
+    ) %>%
+    addPolygons(
+      data = iso_map_valid,
+      fillColor = ~colorNumeric(isochrone_color, iso_map_valid$isomax)(iso_map_valid$isomax),
+      fillOpacity = 0.5,
+      group = "Isochrones",
+      label = ~label,  # Add this line to include labels
+      layerId = ~iso_map_valid$isomax  # Add this line to assign an id to each layer
+    ) %>%
+    addCircles(
+      data = st_coordinates(specified_location),
+      radius = 10,
+      color = location_color,
+      fillOpacity = 1,
+      group = "Specified Location"
+    ) %>%
+    addLayersControl(
+      overlayGroups = c("Green Spaces", "Isochrones", "Specified Location"),
+      options = layersControlOptions(collapsed = TRUE)
+    )
+
+  # Return the map
+  return(map)
 }
